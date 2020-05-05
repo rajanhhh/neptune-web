633f5df40705a3ad9b3e8e5283503f37725f0732
CircleCI <circleci@transferwise.com>
2020-05-05 14:25:17 +0000

    Updates

---
+++ branch/improved-usage-docs/_next/static/RC8JLaP5TGP8OMFFWfnBb/pages/styles/tokens/Spacing.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[64],{Jfwd:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/tokens/Spacing",function(){return o("Yiyu")}])}},[["Jfwd",0,1,2]]]);
