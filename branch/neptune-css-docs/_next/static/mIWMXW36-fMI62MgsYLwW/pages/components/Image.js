c3150ba01c8ea3cf2829f398130f965458e75c7b
CircleCI <circleci@transferwise.com>
2020-05-07 09:42:42 +0000

    Updates

---
+++ branch/neptune-css-docs/_next/static/mIWMXW36-fMI62MgsYLwW/pages/components/Image.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{"1bfL":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Image",function(){return w("E/aF")}])}},[["1bfL",0,1,5,6,2,3,4]]]);
