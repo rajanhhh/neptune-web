474137be188ccfbe1230407b0eb9dd58dc708625
CircleCI <circleci@transferwise.com>
2020-05-15 11:23:26 +0000

    Updates

---
+++ branch/summary-component/_next/static/-fev6IT9ZC-08Af8lvwrk/pages/components/Summary.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{XHeJ:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Summary",function(){return w("mcgz")}])}},[["XHeJ",0,1,4,5,3,2,6,7]]]);
