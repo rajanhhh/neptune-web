0a61c9503c82f632359b418c98fc36eb84759f34
CircleCI <circleci@transferwise.com>
2020-05-28 09:09:19 +0000

    Updates

---
+++ branch/migrate-componenents-on-new-icons-set/_next/static/ylsu_7FcLsAIJn3PDFx8u/pages/components/JsonSchemaForm.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{hyQj:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/JsonSchemaForm",function(){return w("BKGX")}])}},[["hyQj",0,1,2,6,3,5,7,8,9,11,10,12,13,14,17,4]]]);
