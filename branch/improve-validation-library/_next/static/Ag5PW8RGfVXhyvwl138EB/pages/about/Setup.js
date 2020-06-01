66c784394b46a6c7a7c42a3a07096912e1ade422
CircleCI <circleci@transferwise.com>
2020-06-01 16:31:55 +0000

    Updates

---
+++ branch/improve-validation-library/_next/static/Ag5PW8RGfVXhyvwl138EB/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,3,2,4,5,6,7,10,9,8,11,12,15,13,21,22,20,16,17,18,14,19,23,24]]]);
