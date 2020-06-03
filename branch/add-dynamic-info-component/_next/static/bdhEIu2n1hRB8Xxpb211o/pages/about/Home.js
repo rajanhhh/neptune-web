3dd148cb8d98d6b4b8719cdffac5698a73e89ba7
CircleCI <circleci@transferwise.com>
2020-06-03 22:57:41 +0000

    Updates

---
+++ branch/add-dynamic-info-component/_next/static/bdhEIu2n1hRB8Xxpb211o/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,8,10,9,11,12,13,21,20,16,14,19,17,18,22,15,23,24]]]);
