040c7131891d1154b6487e3f3eeb893eac839de2
CircleCI <circleci@transferwise.com>
2020-06-05 13:37:27 +0000

    Updates

---
+++ branch/migrate-componenents-on-new-icons-set/_next/static/sXPj1qtbANSZT7aS9czS-/pages/components/Field.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[51],{FWqW:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Field",function(){return w("lQrX")}])}},[["FWqW",0,1,2,5,3,4,6,7,8,10,11,9,12,13,21]]]);
