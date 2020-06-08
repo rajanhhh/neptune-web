b07332d376db343b1c4c1dbcf6477293c89f3306
CircleCI <circleci@transferwise.com>
2020-06-08 11:02:10 +0000

    Updates

---
+++ branch/DS-275-avatar-component/_next/static/LwVLNdHjxDZW6VHp1KpGD/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,9,10,8,11,12,15,21,19,16,14,20,17,22,13,18,23,24]]]);
