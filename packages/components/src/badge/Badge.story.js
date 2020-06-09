47f83b5d57c145b3fb07b314395fd9093afa3818
Alejandro Alvarez <alejandro.alvarez@transferwise.com>
2020-06-09 10:42:41 +0100

    feat: Add Badge component

---
+++ packages/components/src/badge/Badge.story.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Badge from './Badge';
+import { action } from '@storybook/addon-actions';
+import { boolean, select, text } from '@storybook/addon-knobs';
+
+export default {
+  component: Badge,
+  title: 'Badge',
+};
+
+export const basic = () => {
+  const BadgeInnerText = text('Badge Inner Text', 'Content');
+  const BadgeText = text('BadgeText', 'B');
+  return (
+    <Badge badge={ <div style={{ background: 'var(--color-positive)', width: '100%', height: '100%', color: 'white', fontSize: '0.8em' }}>{BadgeText}</div> }>
+      <div style={{ background: 'var(--color-secondary)', color: 'white', padding: '0.5em 1em' }}>
+        {BadgeInnerText}
+      </div>
+    </Badge>
+  );
+};
