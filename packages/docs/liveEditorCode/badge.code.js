aca9c465ea89de591823c2fd0e7d16034043c23d
Alejandro Alvarez <alejandro.alvarez@transferwise.com>
2020-06-09 14:45:55 +0100

    fix: badge docs

---
+++ packages/docs/liveEditorCode/badge.code.js
@@ -0,0 +1,7 @@
+<Badge
+  badge={ <div style={{ background: 'var(--color-positive)', width: '100%', height: '100%', color: 'white', fontSize: '0.8em' }}>B</div> }
+>
+  <div style={{ background: 'var(--color-secondary)', color: 'white', padding: '0.5em 1em' }}>
+    Content
+  </div>
+</Badge>
