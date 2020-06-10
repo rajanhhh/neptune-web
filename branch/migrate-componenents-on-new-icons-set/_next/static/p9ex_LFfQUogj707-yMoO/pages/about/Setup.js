39f3ac967735c8e5c7b54811e47d2a51304376cf
CircleCI <circleci@transferwise.com>
2020-06-10 21:24:31 +0000

    Updates

---
+++ branch/migrate-componenents-on-new-icons-set/_next/static/p9ex_LFfQUogj707-yMoO/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,5,3,4,6,7,8,10,11,9,12,13,17,15,21,16,22,20,19,14,18,23,24]]]);
