0d25e97d6b950155b7dd3cb5f2bb051a7dd34b7c
CircleCI <circleci@transferwise.com>
2020-06-22 09:51:08 +0000

    Updates

---
+++ branch/DS-275-avatar-component/_next/static/C0Q06OcckKzXuOLUZuPBB/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,8,10,11,12,13,15,16,18,17,19,22,14,20,21,23,24]]]);
