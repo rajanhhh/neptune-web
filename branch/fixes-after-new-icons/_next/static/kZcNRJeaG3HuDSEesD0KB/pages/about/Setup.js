7e2e67d5ce0b616a1596d7f57283e4c15727d3ae
CircleCI <circleci@transferwise.com>
2020-06-24 12:08:31 +0000

    Updates

---
+++ branch/fixes-after-new-icons/_next/static/kZcNRJeaG3HuDSEesD0KB/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,5,3,4,6,7,8,10,9,11,12,13,16,21,15,14,20,22,19,18,17,23,24]]]);
