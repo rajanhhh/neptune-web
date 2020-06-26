a37f5f04110a90015c320f8942c5a819d35eda83
CircleCI <circleci@transferwise.com>
2020-06-26 13:10:11 +0000

    Updates

---
+++ branch/remove-bootstrap-variables/_next/static/sV7E3sBvLjsZaMRZU91ni/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,9,10,11,12,20,18,22,14,17,19,16,15,13,21,23,24]]]);
