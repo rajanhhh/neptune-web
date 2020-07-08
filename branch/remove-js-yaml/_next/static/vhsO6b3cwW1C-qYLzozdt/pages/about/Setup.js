1d0eb364a16f3c14de43e6a99674c3019cc0c7a6
CircleCI <circleci@transferwise.com>
2020-07-08 11:04:25 +0000

    Updates

---
+++ branch/remove-js-yaml/_next/static/vhsO6b3cwW1C-qYLzozdt/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,10,8,11,12,15,18,14,13,22,20,19,17,21,16,23,24]]]);
