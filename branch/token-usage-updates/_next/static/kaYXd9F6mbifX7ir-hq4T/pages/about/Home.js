4390f0541fb2c86d05c38ec390a8190a31b340b9
CircleCI <circleci@transferwise.com>
2020-07-13 16:19:54 +0000

    Updates

---
+++ branch/token-usage-updates/_next/static/kaYXd9F6mbifX7ir-hq4T/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,10,8,9,11,12,14,13,17,15,19,22,16,21,20,18,23,24]]]);
