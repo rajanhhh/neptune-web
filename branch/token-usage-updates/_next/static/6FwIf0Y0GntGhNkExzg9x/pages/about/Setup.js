7180d3640d4f169bc33223a4cb1f9c801099ece4
CircleCI <circleci@transferwise.com>
2020-07-13 10:21:10 +0000

    Updates

---
+++ branch/token-usage-updates/_next/static/6FwIf0Y0GntGhNkExzg9x/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,8,10,11,12,13,20,22,21,14,15,18,19,17,16,23,24]]]);
