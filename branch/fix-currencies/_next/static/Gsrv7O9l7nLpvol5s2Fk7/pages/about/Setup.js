dd4f94a4235a829295af161cd703c25c0680c285
CircleCI <circleci@transferwise.com>
2020-07-14 15:31:54 +0000

    Updates

---
+++ branch/fix-currencies/_next/static/Gsrv7O9l7nLpvol5s2Fk7/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,10,9,11,12,13,20,15,14,17,22,21,16,18,19,23,24]]]);
