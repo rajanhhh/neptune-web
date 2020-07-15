080ec844d093290965e6c6da358a27f7c451e4ca
CircleCI <circleci@transferwise.com>
2020-07-15 12:03:43 +0000

    Updates

---
+++ branch/dev/_next/static/q8F7O6bNcPFr7xO2x-6N5/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,9,10,11,12,22,19,16,17,18,15,20,21,13,14,23,24]]]);
