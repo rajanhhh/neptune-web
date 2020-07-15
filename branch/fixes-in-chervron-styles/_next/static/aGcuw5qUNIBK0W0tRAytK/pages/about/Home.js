403040343670a90efb4187aa911e132c503ab842
CircleCI <circleci@transferwise.com>
2020-07-15 14:44:46 +0000

    Updates

---
+++ branch/fixes-in-chervron-styles/_next/static/aGcuw5qUNIBK0W0tRAytK/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,5,3,4,6,7,8,10,9,11,12,13,19,22,20,18,14,21,16,17,15,23,24]]]);
