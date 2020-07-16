a83ae7c67d6d360541e152a3d99df5d5402011f3
CircleCI <circleci@transferwise.com>
2020-07-16 08:05:33 +0000

    Updates

---
+++ branch/DS-468-badge-component/_next/static/y0wBBHSDwj5iiJjzgJtBe/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,8,9,10,11,12,13,15,14,17,18,16,19,22,20,21,23,24]]]);
