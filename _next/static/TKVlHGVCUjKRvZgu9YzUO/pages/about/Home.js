3f8eb017b0fcc1efa4dd4e671b3d2c92c89ddce7
CircleCI <circleci@transferwise.com>
2020-07-17 13:00:47 +0000

    Updates

---
+++ _next/static/TKVlHGVCUjKRvZgu9YzUO/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,8,9,10,11,12,22,15,19,18,14,20,21,17,13,16,23,24]]]);
