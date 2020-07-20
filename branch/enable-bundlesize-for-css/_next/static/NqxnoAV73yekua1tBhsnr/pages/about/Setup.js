c546296e8c7a80d5a14b8b733278a08f2c932c84
CircleCI <circleci@transferwise.com>
2020-07-20 13:54:31 +0000

    Updates

---
+++ branch/enable-bundlesize-for-css/_next/static/NqxnoAV73yekua1tBhsnr/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,9,10,11,12,16,21,19,13,15,22,17,14,20,18,23,24]]]);
