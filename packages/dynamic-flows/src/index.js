0840bf983a53d1ab2a87d50ab5ad52f04b13159f
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-07-21 17:14:46 +0200

    chore: exposed type-validator

--- packages/dynamic-flows/src/index.js
+++ packages/dynamic-flows/src/index.js
@@ -1,3 +1,13 @@
 export { default as JsonSchemaForm } from './jsonSchemaForm';
 export { default as FormControl } from './formControl';
 export { default as Field } from './field';
+export {
+  isString,
+  isNumber,
+  isInteger,
+  isBoolean,
+  isObject,
+  isArray,
+  isNull,
+  isUndefined,
+} from './common/validation/type-validators';
