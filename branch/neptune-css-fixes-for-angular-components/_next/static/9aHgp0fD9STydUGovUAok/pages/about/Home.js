50c49ade4335c5cc8f20f4e0a8e0f274e40032e6
CircleCI <circleci@transferwise.com>
2020-07-21 11:10:51 +0000

    Updates

---
+++ branch/neptune-css-fixes-for-angular-components/_next/static/9aHgp0fD9STydUGovUAok/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,4,3,5,6,7,8,9,11,10,12,13,17,20,14,21,18,19,15,22,16,23,24]]]);
