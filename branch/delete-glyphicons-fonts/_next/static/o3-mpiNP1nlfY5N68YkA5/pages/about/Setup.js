6c306403b16c8dec2be175212f92f94c181c3eca
CircleCI <circleci@transferwise.com>
2020-07-21 11:43:55 +0000

    Updates

---
+++ branch/delete-glyphicons-fonts/_next/static/o3-mpiNP1nlfY5N68YkA5/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,4,3,5,6,7,8,9,11,10,12,13,19,14,22,16,15,17,18,20,21,23,24]]]);
