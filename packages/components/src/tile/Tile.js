0f0edc93b606f3beed8506d47030f69556a77ab5
Hendrik Mölder <hendrik@molder.eu>
2020-07-22 15:12:31 +0300

    refactor(tile): add todos

--- packages/components/src/tile/Tile.js
+++ packages/components/src/tile/Tile.js
@@ -6,7 +6,17 @@ import './Tile.css';
 
 export const Tile = ({ as: Element, className, title, content, illustration, onClick }) => {
   return (
-    <Element className={classNames('tile', className)} onClick={onClick}>
+    <Element
+      className={classNames('tile', className)}
+      onClick={onClick}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === 'Spacebar') {
+          onClick();
+        }
+      }}
+      tabIndex="0"
+      role="button"
+    >
       {/* TODO: Do we need to set a max height for the illustration? */}
       <div className="m-y-2">{illustration}</div>
       <h4 className="m-b-1">{title}</h4>
@@ -25,7 +35,7 @@ Tile.propTypes = {
 };
 
 Tile.defaultProps = {
-  as: 'div', // TODO: clickable div – accessibility?
+  as: 'div', // TODO: clickable div – accessibility? should it be a button? or a link? Does Button support having illustrations?
   illustration: null,
   content: null,
   className: null,
