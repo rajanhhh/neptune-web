673c8482bb9a6743e670f2d197fbcbba09426199
CircleCI <circleci@transferwise.com>
2020-07-22 16:28:57 +0000

    Updates

---
+++ branch/dynamic-select-improvements/_next/static/4TymWkpKiMg2UKJ6rJMPR/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,9,8,10,11,12,15,16,14,20,18,22,21,19,17,13,23,24]]]);
---
+++ branch/dynamic-select-improvements/_next/static/m844turmEqXgtnowLS4LU/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,9,10,11,12,21,20,22,13,14,16,17,19,18,15,23,24]]]);
