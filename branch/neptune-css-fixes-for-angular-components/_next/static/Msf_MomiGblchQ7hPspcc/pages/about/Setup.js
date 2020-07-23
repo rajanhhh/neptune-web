b72b82a9cede56bbd8a15e469cba83dd3c2f5dfd
CircleCI <circleci@transferwise.com>
2020-07-23 13:36:48 +0000

    Updates

---
+++ branch/neptune-css-fixes-for-angular-components/_next/static/Msf_MomiGblchQ7hPspcc/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,4,3,5,6,7,8,9,11,10,12,13,14,21,19,18,15,16,17,20,22,23,24]]]);
