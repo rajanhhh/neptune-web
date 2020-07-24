afca6e0696780700390b6eef91654ae62c3a61b6
CircleCI <circleci@transferwise.com>
2020-07-24 13:55:17 +0000

    Updates

---
+++ branch/select-css-fixes/_next/static/6xL5D_chBsUDZ3_FmtGuO/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,5,3,4,6,7,8,9,10,11,12,13,22,17,15,21,18,16,20,14,19,23,24]]]);
