9bece2ce8e4469b47d521c81f564cd6fa0094a19
CircleCI <circleci@transferwise.com>
2020-07-24 13:09:45 +0000

    Updates

---
+++ branch/REC-1178_add-tile/_next/static/5snB9AZqaYlYVI8JTMwy6/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,3,4,5,6,7,10,8,9,11,12,16,14,19,20,15,13,18,22,21,17,23,24]]]);
