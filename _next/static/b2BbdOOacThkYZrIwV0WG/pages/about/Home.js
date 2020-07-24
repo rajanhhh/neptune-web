8ce4810c874bb9829a779d6e6be8b04b6b16c385
CircleCI <circleci@transferwise.com>
2020-07-24 15:13:00 +0000

    Updates

---
+++ _next/static/b2BbdOOacThkYZrIwV0WG/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,3,2,4,5,6,7,9,10,8,11,12,19,13,17,21,20,16,22,18,15,14,23,24]]]);
---
+++ _next/static/b2BbdOOacThkYZrIwV0WG/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,3,2,4,5,6,7,9,10,8,11,12,19,13,17,21,20,16,22,18,15,14,23,24]]]);
