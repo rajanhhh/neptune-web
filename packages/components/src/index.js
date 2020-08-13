c67f36f2757368e9f23c6c8aa4a5353e258a2f16
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-08-13 16:14:17 +0200

    refactor: removed old field BREAKING CHANGE: removed old field

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -49,3 +49,4 @@ export { default as Markdown } from './markdown';
 export { default as Card } from './card';
 export { default as Summary } from './summary';
 export { default as Switch } from './switch';
+export { default as Field } from './field';
