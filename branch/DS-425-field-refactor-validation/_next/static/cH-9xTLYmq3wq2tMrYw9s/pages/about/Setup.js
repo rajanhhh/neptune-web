6c947b22bac940d18473cb8f6b58564f7f52fedd
CircleCI <circleci@transferwise.com>
2020-08-13 12:35:50 +0000

    Updates

---
+++ branch/DS-425-field-refactor-validation/_next/static/cH-9xTLYmq3wq2tMrYw9s/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,8,10,9,11,12,19,22,13,15,18,14,20,17,21,16,23,24]]]);
---
+++ branch/DS-425-field-refactor-validation/_next/static/vhPLVyo95k2VK771lKxng/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,8,10,11,12,22,13,19,21,20,15,16,14,18,17,23,24]]]);
