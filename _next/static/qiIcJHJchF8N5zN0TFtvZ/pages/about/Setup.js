cafe37e0e48f51fb04afd8aeac09a288a0747677
CircleCI <circleci@transferwise.com>
2020-08-18 10:51:27 +0000

    Updates

---
+++ _next/static/qiIcJHJchF8N5zN0TFtvZ/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,10,8,11,12,17,20,22,19,15,18,13,21,16,14,23,24]]]);
