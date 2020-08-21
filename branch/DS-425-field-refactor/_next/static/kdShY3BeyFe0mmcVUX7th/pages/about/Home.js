55539921699eb5f28f71255e3fe5440c606f39f8
CircleCI <circleci@transferwise.com>
2020-08-21 09:13:44 +0000

    Updates

---
+++ branch/DS-425-field-refactor/_next/static/kdShY3BeyFe0mmcVUX7th/pages/about/Home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{UDZ2:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Home",function(){return w("faqb")}])}},[["UDZ2",0,1,2,4,3,5,6,7,8,9,11,10,12,13,22,19,20,14,21,17,18,16,15,23,24]]]);
