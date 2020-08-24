f0f5c5bf577c8f2b4bea4064db94b54b7cad3416
CircleCI <circleci@transferwise.com>
2020-08-24 10:15:00 +0000

    Updates

---
+++ branch/css-variables/_next/static/9CzQpJYIhGJc6Z8lQte3K/pages/styles/core/Typography.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[83],{pN0W:function(n,o,p){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/core/Typography",function(){return p("BTr6")}])}},[["pN0W",0,1,2,3,4,5,6,7,8,9,10,11,12,14,19,17,15,16,21,20,13,18,22,23]]]);
---
+++ branch/css-variables/_next/static/9CzQpJYIhGJc6Z8lQte3K/pages/styles/tokens/Spacing.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[86],{Jfwd:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/tokens/Spacing",function(){return o("Yiyu")}])}},[["Jfwd",0,1,2,3,4,5,6,7,8,9,10,11,12,14,19,17,15,16,21,20,13,18,22,23]]]);
