0fd6feca0204d4453e00061ba5750802c1747e06
CircleCI <circleci@transferwise.com>
2020-08-27 06:08:54 +0000

    Updates

---
+++ branch/fix-min-max-validation/_next/static/PUNcwMYkpwSkIoDxThwep/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,8,10,11,12,22,14,15,20,21,13,18,17,19,16,23,24]]]);
