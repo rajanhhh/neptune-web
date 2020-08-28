52220cbad217792dbc24a6c9667e13e252560b92
CircleCI <circleci@transferwise.com>
2020-08-28 09:12:31 +0000

    Updates

---
+++ branch/ADS-1539_empty_validation_on_optional_field/_next/static/Q5S0TR3Cxpx0GifGwzZ36/pages/about/Setup.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{HHz7:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return o("e66W")}])}},[["HHz7",0,1,2,3,4,5,6,7,9,8,10,11,12,19,21,22,18,14,20,13,16,17,15,23,24]]]);
